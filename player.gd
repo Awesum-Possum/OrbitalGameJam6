9b8e1ea450a43ff7c197778f10e0c670ea511a1b
Victor Schneuwly <victor.schneuwly@epfl.ch>
2024-03-09 13:59:47 +0100

    Update player

--- player.gd
+++ player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 @export var speed = 1200
 @export var jump_speed = -1800
 @export var gravity = 4000
-@export var jump_velocity = -400
+@export var jump_velocity = 400
 
 
 func _physics_process(delta):
@@ -13,7 +13,8 @@ func _physics_process(delta):
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_velocity
+		# minus since jump up is negative
+		velocity.y = -jump_velocity
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
