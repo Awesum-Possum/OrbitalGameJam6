f05a4984e836d9aa6684e8f99b7e8628c3990ce1
Victor Schneuwly <victor.schneuwly@epfl.ch>
2024-03-09 23:10:53 +0100

    Use % for player get

--- scripts/wall_of_death.gd
+++ scripts/wall_of_death.gd
@@ -1,12 +1,11 @@
 extends Area2D
 
-
 @export var speed = 400
-@export var player: Node2D = null
 
-var game_over = preload('res://game_over.tscn')
+var game_over = preload("res://game_over.tscn")
 
 @onready var dogs = $Dogs
+@onready var player: Player = %Player
 
 
 func _process(delta):
@@ -14,12 +13,12 @@ func _process(delta):
 	dogs.move(player.position.y)
 
 
-func _on_body_entered(body:Node2D):
+func _on_body_entered(body: Node2D):
 	if body is Player:
 		Globals.game_over.emit()
 		speed = 3000
-		get_tree().create_timer(0.5)\
-			.timeout.connect(show_game_over)
+		get_tree().create_timer(0.5).timeout.connect(show_game_over)
+
 
 func show_game_over():
 	speed = 0
