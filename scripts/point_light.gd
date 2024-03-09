c24816bb9c39529c13984f24b5ea8864a7ace9b8
Victor Schneuwly <victor.schneuwly@epfl.ch>
2024-03-09 22:38:03 +0100

    Finish animation of stele with white screen

--- scripts/point_light.gd
+++ scripts/point_light.gd
@@ -1,10 +1,13 @@
 class_name MyLight extends Node2D
 
+## The time it takes for the light to decay
 @export var decay_time = 10
+## The minimum energy the light can have
 @export var min_energy = 0.1
+## Whether the light should decay or not
 @export var decays = false
 
-var current_time = 0
+var _current_time = 0
 
 @onready var light1: PointLight2D = $TextureLight
 @onready var light2: PointLight2D = $Shadow
@@ -16,17 +19,26 @@ var current_time = 0
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if decays:
-		current_time += delta
+		_current_time += delta
 		light1.energy = max(
-			initial_light1 - (current_time / decay_time) * initial_light1, min_energy
+			initial_light1 - (_current_time / decay_time) * initial_light1, min_energy
 		)
 		light2.energy = max(
-			initial_light2 - (current_time / decay_time) * initial_light2, min_energy
+			initial_light2 - (_current_time / decay_time) * initial_light2, min_energy
 		)
 
 
 ## Reset the light to their initial values
 func reset():
-	current_time = 0
+	_current_time = 0
 	light1.energy = initial_light1
 	light2.energy = initial_light2
+	decays = true
+
+
+## Turn off the light
+func turn_off():
+	light1.enabled = false
+	light2.enabled = false
+	decays = false
+	visible = false
