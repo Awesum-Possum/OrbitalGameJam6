bc862d03134f9188c8275dc8f285e68560a7b242
Victor Schneuwly <victor.schneuwly@epfl.ch>
2024-03-09 21:14:11 +0100

    Complete animation of the stele

--- scripts/stele.gd
+++ scripts/stele.gd
@@ -1,14 +1,22 @@
-extends Area2D
-
-class_name Stele
-
-@onready var light = $Light
+class_name Stele extends Area2D
 
 var consumed = false
 
+@onready var light: MyLight = $Light
+@onready var animation: AnimatedSprite2D = $AnimatedSprite2D
 
-func _on_body_entered(body:Node2D):
-	if body.is_in_group("player") and not consumed:
-		consumed = true
-		body.regen_light()
-		light.visible = false
+
+func _on_body_entered(body: Node2D):
+	var player = body as Player
+	if not player or consumed:
+		return
+
+	consumed = true
+	player.regen_light()
+	light.visible = false
+	animation.play("explosion")
+
+
+func _on_animation_finished():
+	if animation.animation.contains("explosion"):
+		animation.play("dead")
