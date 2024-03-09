bcbb07562acca79ab78bb6f9e5792ce96d3c1488
Victor Schneuwly <victor.schneuwly@epfl.ch>
2024-03-09 13:41:13 +0100

    First setup of player

---
+++ player.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+@export var speed = 1200
+@export var jump_speed = -1800
+@export var gravity = 4000
+@export var jump_velocity = -400
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_velocity
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("walk_left", "walk_right")
+	if direction:
+		velocity.x = direction * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+
+	move_and_slide()
