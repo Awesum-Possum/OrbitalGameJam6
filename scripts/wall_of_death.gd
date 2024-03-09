f4cc2bee6fd25d9125c43cfbf1a445f64b2b4ce4
Victor Schneuwly <victor.schneuwly@epfl.ch>
2024-03-10 00:16:36 +0100

    Update wall of death

--- scripts/wall_of_death.gd
+++ scripts/wall_of_death.gd
@@ -1,16 +1,31 @@
-extends Area2D
+class_name WallOfDeath extends Area2D
 
 @export var speed = 400
+## The gap, in pixel, between each mob
+@export var mob_gab = 35
+## The number of frames in the mob animation
+@export var nb_frames = 7
 
+var dog_scene := preload("res://doggo.tscn")
 var game_over = preload("res://game_over.tscn")
 
-@onready var dogs = $Dogs
 @onready var player: Player = %Player
+@onready var mobs: Node2D = $Mobs
+@onready var mesh: MeshInstance2D = $MeshInstance2D
+
+
+func _ready():
+	var start_pos = mesh.scale.y / 2
+	for i in range(0, mesh.scale.y, mob_gab):
+		var dog = dog_scene.instantiate()
+		dog.position.y = start_pos - i
+		dog.frame = randi() % nb_frames
+		mobs.add_child(dog)
 
 
 func _process(delta):
 	position.x += speed * delta
-	dogs.move(player.position.y)
+	mobs.move(player.position.y)
 
 
 func _on_body_entered(body: Node2D):
