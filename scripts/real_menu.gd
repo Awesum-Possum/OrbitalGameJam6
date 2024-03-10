bc908ded18d3108d01b82fdedd5e27664add7573
Victor Schneuwly <victor.schneuwly@epfl.ch>
2024-03-10 04:56:07 +0100

    Add new main scene

--- scripts/real_menu.gd
+++ scripts/real_menu.gd
@@ -3,18 +3,9 @@ extends CanvasLayer
 @onready var main = $Control/PanelContainer/MarginContainer/VBoxContainer
 @onready var credits = $Control/PanelContainer/MarginContainer/Credits
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 
 func _on_run_pressed():
-	get_tree().change_scene_to_file("res://initial_scene.tscn")
+	get_tree().change_scene_to_file(Globals.MAIN_SCENE_PATH)
 
 
 func _on_credits_pressed():
@@ -27,6 +18,7 @@ func _on_quit_pressed():
 
 func _on_back_pressed():
 	show_and_hide(main, credits)
+
 
 func show_and_hide(first, second):
 	first.show()
