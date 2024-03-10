6087c50c736d0e8dce4218b36e012197b56d4c4c
Victor Schneuwly <victor.schneuwly@epfl.ch>
2024-03-10 03:43:01 +0100

    Add boing to jump platform

--- scripts/jump_platform.gd
+++ scripts/jump_platform.gd
@@ -3,8 +3,10 @@ extends StaticBody2D
 ## The force applied to the player when jumping.
 @export var jump_force = 1000
 
+@onready var _audio: AudioStreamPlayer = $AudioStreamPlayer
+
 
 func _on_body_entered(body: Node2D) -> void:
-	print("JUMP!")
 	if body is Player:
 		body.jump(jump_force)
+		_audio.play()
