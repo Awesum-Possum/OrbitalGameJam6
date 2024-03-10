226e11e3c70ac73da4657a162008066beee8076d
Victor Schneuwly <victor.schneuwly@epfl.ch>
2024-03-10 06:59:58 +0100

    Finalize spider

--- scripts/spider.gd
+++ scripts/spider.gd
@@ -6,8 +6,10 @@ var _goes_down: bool = false
 var _spawn_pos: Vector2
 var _platform_pos: Vector2
 var _over: bool = false
+var _previous_web_pos: Vector2
 
 var _game_over = preload("res://game_over.tscn")
+var _web_string = preload("res://web_string.tscn")
 
 
 func _ready():
@@ -18,10 +20,12 @@ func _ready():
 func _process(delta):
 	if _goes_down and position.y < _platform_pos.y:
 		position.y += speed * delta
+		if not _previous_web_pos or position.y - _previous_web_pos.y > 16:
+			spawn_web()
+			_previous_web_pos = position
+
 	elif _goes_down and not _over and position.y >= _platform_pos.y:
 		_goes_down = false
-	elif position.y < _spawn_pos.y:
-		position.y += speed * delta
 	elif position.y > _spawn_pos.y:
 		position.y -= speed * delta
 	else:
@@ -50,3 +54,11 @@ func _on_body_entered(body: Node2D) -> void:
 	player.visible = false
 	player.speed = 0
 	_over = true
+
+
+func spawn_web():
+	print("spawning web at", position)
+	var web = _web_string.instantiate()
+	web.position = position
+	web.z_index = z_index - 1
+	get_tree().root.add_child(web)
